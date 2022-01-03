a2ab0a409ac06a4a3b26a92accfb399281620507
rgiovannyrdj <97043543+rgiovannyrdj@users.noreply.github.com>
2022-01-03 11:10:23 -0600

    Update ConnectionProps.cs

--- TODOLIST.INFRASTRUCTURE/Data/ConnectionProps.cs
+++ TODOLIST.INFRASTRUCTURE/Data/ConnectionProps.cs
@@ -3,6 +3,6 @@
     public static class ConnectionProps
     {
         //public static string ConnectionString = "mongodb+srv://dev-roberto:Vkd1UlfTHJvG2GuK@dev-isol.k7u18.mongodb.net/todo_roberto?retryWrites=true&w=majority";
-        public static string ConnectionString = "mongodb+srv://rgiovanny:Eminemn1@cluster0.pifgn.mongodb.net/test?authSource=admin&replicaSet=atlas-10mxug-shard-0&readPreference=primary&ssl=true";
+        public static string ConnectionString = "mongodb+srv://rgiovanny:<Password>@cluster0.pifgn.mongodb.net/test?authSource=admin&replicaSet=atlas-10mxug-shard-0&readPreference=primary&ssl=true";
     }
 }
